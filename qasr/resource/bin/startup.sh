45ca9cc08226e911c18147be71a620222b6fc581
xiphoid <xiphoid@xiphoid-PC>
2013-05-25 02:44:43 +0900

    r

--- qasr/resource/bin/startup.sh
+++ qasr/resource/bin/startup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-export JAVA_HOME="/home/interest/install/java"
-/usr/bin/killall -9 java
-sleep 1
-cd /home/interest/server/dbproxy/bin
 export JAVA_OPTS="-server  -Xmx2048m -Xms2048m -verbose:gc -XX:+UseCompressedOops -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseFastAccessorMethods"
-$JAVA_HOME/bin/java $JAVA_OPTS -cp "../conf:$(echo ../lib/*.jar | tr ' ' ':')"  me.interest.pi.relay.Main 2>&1 > /tmp/log &
+$JAVA_HOME/bin/java $JAVA_OPTS -cp "../conf:$(echo ../lib/*.jar | tr ' ' ':')"  com.qasr.Main 2>&1 > /tmp/log &
