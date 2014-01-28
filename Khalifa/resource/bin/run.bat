a4c02c547b5c964ca946eb828319a2534e5b2f63
xiphoid <xiphoid@52.80.154.87>
2014-01-28 12:01:12 +0900

    test

---
+++ Khalifa/resource/bin/run.bat
@@ -0,0 +1,4 @@
+
+set JAVA_OPTS="-server  -Xmx2048m -Xms2048m -verbose:gc -XX:+UseCompressedOops -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled "
+
+java -server -cp ..\conf -Djava.ext.dirs=..\lib com.cj.enm.platform.dbproxy.Main
---
+++ Khalifa/resource/bin/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export JAVA_HOME="/home/interest/install/java"
+/usr/bin/killall -9 java
+sleep 1
+cd /home/interest/server/dbproxy/bin
+export JAVA_OPTS="-server  -Xmx2048m -Xms2048m -verbose:gc -XX:+UseCompressedOops -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseFastAccessorMethods"
+$JAVA_HOME/bin/java $JAVA_OPTS -cp "../conf:$(echo ../lib/*.jar | tr ' ' ':')"  com.cj.enm.platform.dbproxy.Main 2>&1 > /tmp/log &
