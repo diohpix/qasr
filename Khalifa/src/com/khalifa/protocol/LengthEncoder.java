9bc3128503c398279a7e4007999eb1c6106e7dd8
xiphoid <xiphoid@52.80.154.87>
2014-02-14 17:53:33 +0900

    logger

--- Khalifa/src/com/khalifa/protocol/LengthEncoder.java
+++ Khalifa/src/com/khalifa/protocol/LengthEncoder.java
@@ -5,11 +5,15 @@ import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Sharable
 public class LengthEncoder extends MessageToByteEncoder<ByteBuf> {
+	private final Logger log = LoggerFactory.getLogger(LengthEncoder.class);
 	@Override
 	protected void encode( ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) throws Exception {
-		//System.out.println("final Encoder "+ msg.readableBytes());
+		log.debug("final Encoder {}", msg.readableBytes());
 		int bodyLen = msg.readableBytes();
 		Object g = ctx.pipeline().get("___INRERNAL_GZIP___");
 		if(g!=null){
